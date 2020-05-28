e75c98c0acbfde4778e31ce8efc51db615a6b713
rossgrams <ross.grams@gmail.com>
2020-05-28 15:23:31 -0400

    Menu-Items,Test: Move visual tests & examples 1st in menu. Fix typo in test label.

--- main/menu-items.lua
+++ main/menu-items.lua
@@ -4,16 +4,16 @@
 -- { buttonName, callbackName, { arg1, arg2 } },
 
 local M = {
-	{ "Unit Tests", "openChildren", {
-	} },
-	{ "Performance Tests", "openChildren", {
-	} },
 	{ "Visual Tests", "openChildren", {
 		{ "starting transform", "loadScene", {"starting transform"} },
 		{ "animate aspect ratio", "loadScene", {"animate aspect ratio"} },
 		{ "get camera by url", "loadScene", {"get camera by url"} },
 	} },
 	{ "Examples", "openChildren", {
+	} },
+	{ "Unit Tests", "openChildren", {
+	} },
+	{ "Performance Tests", "openChildren", {
 	} },
 }
 
